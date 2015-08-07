ddebb6292dcfc72e982bd8128b0415c0e1a06c69
Lars Walther <meinkraeks@gmail.com>
2015-08-07 13:55:56 +0200

    Extension pdf-Druck eingebaut

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,9 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.fulltoc']
+extensions = ['sphinxcontrib.fulltoc', 'rst2pdf.pdfbuilder']
+
+pdf_documents = [('index', u'kraeks_full', u'PDF-Dokumention des Kraeks-Projekts', u'Franz Lampe & Lars Walther'),]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
